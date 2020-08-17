8f6708c6dfdd6b3c67de8cef5ded39c168623891
hamohuh <hamodroid@gmail.com>
2020-08-17 23:50:43 +0200

    This component load my profile

---
+++ client/pages/users/MyProfile.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { initializeApollo } from "../../lib/apollo";
+import { useQuery } from "@apollo/client";
+import Layout from "../../components/Layout";
+import ErrorMessage from "../../components/ToastMessage";
+import { ONE_USER_QUERY, ME_QUERY } from "../../graphql/queries";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import DropZoneField from "../../components/DropZoneField";
+
+function MyProfile(props) {
+  let me = props.me;
+
+  return (
+    <>
+      {me && (
+        <>
+          <Head>
+            <title>My Profile</title>
+            <link rel='icon' href='/favicon.ico' />
+          </Head>
+          <div>
+            <h3>welcome {me.userName}</h3>
+            <div>{me.id}</div>
+            <div>{me.meName}</div>
+            <div>{me.email}</div>
+            <div>{me.firstName}</div>
+            <div>{me.lastName}</div>
+            <div>{me.createdAt}</div>
+            <div>{me.createdAt}</div>
+            {me?.pictures.length > 0 &&
+              me.pictures.map((pic, index) => (
+                <img key={index} src={`/images/users_images/${pic}`} />
+              ))}
+          </div>
+          <DropZoneField />
+        </>
+      )}
+    </>
+  );
+}
+
+export default MyProfile;
