1a51e0dec8d609c25e5860d1616cf35e9430bf95
hamohuh <hamodroid@gmail.com>
2020-08-29 07:30:00 +0200

    Deploy

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,6 +4,8 @@ var graphql_yoga_1 = require("graphql-yoga");
 var db_1 = require("./db");
 var typeDefs_1 = require("./schema/typeDefs");
 var resolvers_1 = require("./schema/resolvers");
+var path_1 = require("path");
+var express_1 = require("express");
 // Creating our graphQL server with the schema defined
 var server = new graphql_yoga_1.GraphQLServer({
     typeDefs: typeDefs_1["default"],
@@ -19,6 +21,16 @@ var server = new graphql_yoga_1.GraphQLServer({
 });
 // database connection
 db_1.connectDb();
+// serve static assets if in production (heroku configuration)
+if (process.env.NODE_ENV !== "production")
+    require("dotenv").config();
+if (process.env.NODE_ENV == "production") {
+    // set static folder
+    server.express.use(express_1["default"].static(path_1["default"].join(__dirname, "client", "build")));
+    server.get("*", function (req, res) {
+        res.sendFile(path_1["default"].join(__dirname, "client", "build", "index.html"));
+    });
+}
 // Server connection options
 var options = {
     port: 5000,
