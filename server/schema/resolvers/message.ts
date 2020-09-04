a0eb99d626b8494dfbe5d4da1a7e9af9ad707680
hamohuh <hamodroid@gmail.com>
2020-09-04 04:06:14 +0200

    Message resolver

--- server/schema/resolvers/message.ts
+++ server/schema/resolvers/message.ts
@@ -2,39 +2,70 @@ import { userAuth } from "../../middlewares/auth";
 import Chat from "../../models/Chat";
 import Message from "../../models/Message";
 
+const CHAT_CHANNEL = "CHAT_CHANNEL";
+
 const messageResolver = {
-  /*  Query: {
-    oneChatMessages: (_, { chatId }) => Message.find({ chat: chatId })
-  }, */
-  /* Message: {
-    user: (paren: { user: string }) => db.users.find(user => user.id === paren.user)
-  }  */
-  /* Mutation: {
-    sendMessage: async (parent: any, { text, user, chat }, { req, res }) => {
+  /**********  Mutations ************/
+  Mutation: {
+    // send message
+    sendMessage: async (_, { text, user, chat }, { req, pubsub }) => {
       try {
         // 1- authenticate user
-        userAuth(req);
+        await userAuth(req);
 
-        const { userId } = req.user;
+        const userId = req.user.userId;
 
-        if (!chat) {
-          const newChat = new Chat();
-          await newChat.save();
+        // 2- create new message
+        const newMessage = new Message({ text: text, user: user, chat: chat });
+        await newMessage.save();
 
-          const newMessage = new Message({ text: text, user: user, chat: newChat._id });
-          await newMessage.save();
-        }
+        // 3- update the messages array with the new message id
+        await Chat.findByIdAndUpdate(
+          chat,
+          { $push: { messages: newMessage._id } },
+          { useFindAndModify: false }
+        );
+
+        // 4- get the current active chat the user send the message in
+        const currentChat = await Chat.findById(chat)
+          .populate({
+            path: "users",
+            model: "NewUser"
+          })
+          .populate({
+            path: "messages",
+            model: "Message",
+            populate: {
+              path: "user",
+              model: "NewUser"
+            }
+          })
+          .exec();
+
+        // 5- publish the current chat send message subscription
+        pubsub.publish(CHAT_CHANNEL, { userChats: { ok: true, chat: currentChat } });
 
         return {
-          ok: true
+          ok: true,
+          successMessage: "Message sent"
         };
       } catch (error) {
         return {
-          ok: false
+          ok: false,
+          error: error.message
         };
       }
     }
-  } */
+  },
+
+  /************** Subscription ***********/
+  Subscription: {
+    userChats: {
+      subscribe: (_, __, { pubsub }) => {
+        return pubsub.asyncIterator(CHAT_CHANNEL);
+      }
+    }
+  }
 };
 
 export default messageResolver;
