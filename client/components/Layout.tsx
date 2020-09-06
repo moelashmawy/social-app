88845098c942c0a17c00e61ca07f96d271364c53
hamohuh <hamodroid@gmail.com>
2020-09-06 03:13:40 +0200

    Deploy

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./shared/Header";
 import Footer from "./shared/Footer";
+import Head from "next/head";
 
 // out pages layout
 const Layout = props => {
@@ -10,6 +11,12 @@ const Layout = props => {
 
   return (
     <div className='container'>
+      <Head>
+        <link
+          rel='shortcut icon'
+          href='https://spilart.com/wp-content/uploads/2019/04/cim.png'
+        />
+      </Head>
       <Header me={props.me} />
       {props.children}
       <Footer />
