0f4c982cd554b29acab53f6633f1e98d92e8825c
hamohuh <hamodroid@gmail.com>
2020-09-06 02:18:28 +0200

    Deploy

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,3 @@
-const config = {
-  target: "serverless",
-  env: {
-    apiEndpoint:
-      "/graphql" || "http://localhost:5000/graphql" || "https://huhuhu.vercel.app/graphql"
-  }
+module.exports = {
+  target: "serverless"
 };
-
-module.exports = config;
